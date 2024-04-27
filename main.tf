6ba141da31fb53318d0d1237d75f84d6541be3d9
Matthew <mattmeinema@hotmail.com>
2024-04-27 16:54:47 +0200

    First commit of my Azure Cloud Resume Challenge. Includes a static website storage account.

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+    features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+    name     = "WebsiteResourceGroup"
+    location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+    name                     = "meinemawebsitestorage2"
+    resource_group_name      = azurerm_resource_group.rg.name
+    location                 = azurerm_resource_group.rg.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+    account_kind             = "StorageV2"
+
+    static_website {
+        index_document = "index.html"
+        error_404_document = "404.html"
+    }
+}
+
